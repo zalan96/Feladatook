bc181a2b8dd35a3bf9c7979f0343bfa7980f057f
zalan96 <schzalan@gmail.com>
2024-11-05 09:34:07 +0100

    Add project files.

---
+++ Feladatook/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Feladatook
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+
+		static List<int> PrimSzamok (int kezdo, int veg)
+		{
+			List<int> primList = new List<int>();
+		}
+	}
+}
